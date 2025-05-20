55cabcb1c2c1de53510a4330c18fe116fbd2222c
kpast <k.pasternak01@gmail.com>
2025-05-20 14:41:26 +0300

    11

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
+# main.py
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 import pandas as pd
-from pathlib import Path
-from temp import get_main_table
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+table_01 = pd.DataFrame({
+    'A': [1, 2, 3],
+    'B': ['x', 'y', 'z']
+    },
+    index=[11, 22, 33])
+
+
+def get_main_table():
+    # Пример DataFrame
+    df = pd.DataFrame({
+        'A': [1, 2, 3],
+        'B': ['x', 'y', 'z']
+    }, index=[11, 22, 33])
+    # Получаем HTML-таблицу
+    return df.to_html(classes='table table-striped', index=True)
+
 
 @app.get("/", response_class=HTMLResponse)
-async def root(request: Request):
-    df_summary = get_main_table()
-    table_html = df_summary.to_html(classes="table table-striped", index=False, border=0)
-    return templates.TemplateResponse("main.html", {"request": request, "table": table_html})
+async def main(request: Request):
+    table_html = get_main_table()
+    return templates.TemplateResponse(
+        "main.html",
+        {"request": request, "table_html": table_html}
+    )
